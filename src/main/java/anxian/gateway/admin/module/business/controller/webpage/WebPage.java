140799b82b02fbe8ece679b84b8aa6aa8b8568f8
wujianlonglong <956714866@qq.com>
2017-08-31 15:24:08 +0800

    安鲜生活网购-添加页面菜单接口

---
+++ src/main/java/anxian/gateway/admin/module/business/controller/webpage/WebPage.java
@@ -0,0 +1,40 @@
+package anxian.gateway.admin.module.business.controller.webpage;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value="/anXian")
+public class WebPage {
+
+
+    @RequestMapping(value="/good/goodsUpdown",method= RequestMethod.GET)
+    public String goodsUpdown(){
+        return "/anXian-goods/up-down-shelves";
+    }
+
+
+    @RequestMapping(value="/good/goodsTimeUpdown",method= RequestMethod.GET)
+    public String goodsTimeUpdown(){
+        return "/anXian-goods/timer-up-down-shelves";
+    }
+
+
+    @RequestMapping(value="/user/userList",method= RequestMethod.GET)
+    public String userList(){
+        return "/anXian-user/user";
+    }
+
+    @RequestMapping(value="/user/blackList",method= RequestMethod.GET)
+    public String blackList(){
+        return "/anXian-user/blacklist";
+    }
+
+    @RequestMapping(value="/pay/payList",method= RequestMethod.GET)
+    public String payList(){
+        return "/pay/pay";
+    }
+
+}
