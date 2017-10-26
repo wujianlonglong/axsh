2838f398af4d3bcc94462cce9704f82578ea7545
wujianlonglong <956714866@qq.com>
2017-10-26 16:20:12 +0800

    新增短信分页查询接口和重发接口

--- src/main/java/anxian/gateway/admin/module/business/controller/anxian/sms/SmsController.java
+++ src/main/java/anxian/gateway/admin/module/business/controller/anxian/sms/SmsController.java
@@ -2,6 +2,7 @@ package anxian.gateway.admin.module.business.controller.anxian.sms;
 
 
 import client.api.sms.SmsFeign;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +34,27 @@ public class SmsController {
         responseMessage=smsFeign.searchSms(searchParam);
         return responseMessage;
     }
+
+
+    /**
+     * 重发短信
+     * @param smsMsgId 重发的短信id
+     * @return
+     */
+    @RequestMapping("/resendMessage")
+    public ResponseMessage resendMessage(String smsMsgId){
+        ResponseMessage responseMessage=new ResponseMessage();
+        if(StringUtils.isEmpty(smsMsgId)){
+            responseMessage.setFailure(-1, "请求参数的smsMsgId不能为空！", null);
+            return responseMessage;
+        }
+        responseMessage = smsFeign.resendMessage(smsMsgId);
+        return responseMessage;
+    }
+
+
+
+
+
 
 }
--- src/main/java/client/api/sms/SmsFeign.java
+++ src/main/java/client/api/sms/SmsFeign.java
@@ -5,8 +5,9 @@ import anxian.gateway.admin.module.business.controller.anxian.sms.SearchParam;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name="sjes-hub-api",url="http://localhost:20002")
+@FeignClient(name="sjes-hub-api")
 @RequestMapping("/yidong")
 public interface SmsFeign {
 
@@ -18,5 +19,9 @@ public interface SmsFeign {
      */
     @RequestMapping(value="/searchSms")
     ResponseMessage searchSms(@RequestBody SearchParam searchParam);
+
+
+    @RequestMapping("/resendMessage")
+    ResponseMessage resendMessage(@RequestParam("smsMsgId")String smsMsgId);
 
 }
